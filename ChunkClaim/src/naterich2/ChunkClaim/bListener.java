d0140ac5a6069c1ac1164d0b2b7d6efa6b86eda0
Nate Richman <nate.rich2@gmail.com>
2013-01-26 11:22:22 -0600

    Added this Block Listener to see if block has been broken

---
+++ ChunkClaim/src/naterich2/ChunkClaim/bListener.java
@@ -0,0 +1,17 @@
+package naterich2.ChunkClaim;
+import org.bukkit.block.*;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+
+public class bListener implements Listener {
+	
+	private ChunkClaim plugin;
+	
+	public void onBlockBreak(BlockBreakEvent evt){
+		
+	}
+
+}
